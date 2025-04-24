6dfce7093a1bde54bdf0608f452ddc4b886e2dd3
jkamohara3 <jkamohara3@gatech.edu>
2025-04-24 12:35:56 -0400

    fix further warning

--- rl_games/common/a2c_common.py
+++ rl_games/common/a2c_common.py
@@ -259,7 +259,8 @@ class A2CBase(BaseAlgorithm):
         self.mini_epochs_num = self.config['mini_epochs']
 
         self.mixed_precision = self.config.get('mixed_precision', False)
-        self.scaler = torch.cuda.amp.GradScaler(enabled=self.mixed_precision)
+        # self.scaler = torch.cuda.amp.GradScaler(enabled=self.mixed_precision)
+        self.scaler = torch.amp.autocast('cuda', enabled=self.mixed_precision)
 
         self.last_lr = self.config['learning_rate']
         self.frame = 0
